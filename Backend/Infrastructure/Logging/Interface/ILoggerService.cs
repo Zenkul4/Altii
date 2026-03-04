0eb8c5d393e9065f848069e866cd8ddd6e1d3d42
Delis Manuel De La Cruz <delismanuel13@gmail.com>
2026-03-03 22:12:32 -0400

    creacion de las interfaces

---
+++ Backend/Infrastructure/Logging/Interface/ILoggerService.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Logging.Interfaces;
+
+public interface ILoggerService
+{
+    void LogInformation(string message);
+    void LogWarning(string message);
+    void LogError(string message, Exception? exception = null);
+}
---
+++ Backend/Infrastructure/Services/Interface/ICodeGeneratorService.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Services.Interfaces;
+
+public interface ICodeGeneratorService
+{
+    string GenerateBookingCode();
+}
