c7a9abd65742efcfda5fb5cdc92b0cd1e76add70
Enmanuel Chavez <20251099@itla.edu.do>
2026-03-03 22:47:59 -0400

    imeplementations e interfaces hechas

---
+++ Domain/Services/Implementations/RateDomainService.cs
@@ -0,0 +1,16 @@
+﻿using Alti.Domain.Entities;
+using Alti.Domain.Services.Interfaces;
+
+namespace Alti.Domain.Services.Implementations;
+
+public class RateDomainService : IRateDomainService
+{
+    public void UpdatePrice(Rate rate, decimal newPrice)
+    {
+        if (newPrice <= 0)
+            throw new ArgumentException("Price must be greater than zero.", nameof(newPrice));
+
+        rate.PricePerNight = newPrice;
+        rate.UpdatedAt = DateTimeOffset.UtcNow;
+    }
+}
---
+++ Domain/Services/Interfaces/IAdditionalServiceDomainService.cs
@@ -0,0 +1,10 @@
+﻿using Alti.Domain.Entities;
+
+namespace Alti.Domain.Services.Interfaces;
+
+public interface IAdditionalServiceDomainService
+{
+    void UpdatePrice(AdditionalService service, decimal newPrice);
+    void Deactivate(AdditionalService service);
+    void Activate(AdditionalService service);
+}
