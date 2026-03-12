d0573bc746a381d2598be542ae1ba5c7321f4259
Enmanuel Chavez <20251099@itla.edu.do>
2026-03-11 22:00:01 -0400

    validador de servicios

---
+++ Backend/Application/Validators/CreatePaymentValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Dtos.Payment;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class CreatePaymentValidator : AbstractValidator<CreatePaymentDto>
+{
+    public CreatePaymentValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .GreaterThan(0).WithMessage("Booking ID is required.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+
+        RuleFor(x => x.PaymentMethod)
+            .MaximumLength(50).When(x => x.PaymentMethod is not null);
+    }
+}
